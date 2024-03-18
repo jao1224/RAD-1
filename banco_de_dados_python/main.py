0c7771c4305f427b1ce2f54b060cc7b38f5afd4d
jao1224 <fernandojoao072@gmail.com>
2024-03-18 19:57:40 -0300

    v

--- banco_de_dados_python/main.py
+++ banco_de_dados_python/main.py
@@ -1,4 +1,5 @@
 import sqlite3
+from modelo import Pessoa, Marca, Veiculo
 
 banco = sqlite3.connect('database.db')
 banco.execute("PRAGMA foreign_keys=on")
@@ -24,3 +25,13 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Veiculo(
                 PRIMARY KEY(placa),
                 FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
                 FOREIGN KEY(marca) REFERENCES Marca(id));''')
+#cursor.execute('''ALTER TABLE Veiculo ADD motor REAL;''')
+#pessoa=Pessoa(22993083898998, 'pedro', '2000-01-31', True)
+#comando = '''insert into Pessoa(cpf, nome, nascimento, oculos) values'''
+#cursor.execute(comando,(pessoa.cpf,pessoa.nome,pessoa.nascimento,pessoa.usa_oculos))
+pessoas= [Pessoa(122342356745, 'joao', ' 2000-01-31', True  ), Pessoa(102938302089, 'cynthia', '1995-03-10',False)]
+comando= '''INSERT INTO Pessoa(cpf, nome, nascimento, oculos) values (?,?,?,?); '''
+cursor.executemany(comando,[(i.cpf, i.nome,i.nascimento,i.usa_oculos) for i in pessoas])
+banco.commit()
+cursor.close()
+banco.close
